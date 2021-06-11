69d55cf839719a1bda9c3beff42e044b187a56e6
Venkat <85488686+Venkat12310@users.noreply.github.com>
2021-06-11 12:29:02 +0530

    Add files via upload

---
+++ venkat.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 10 23:45:05 2021
+
+@author: Regula Malakondaiah
+"""
+
+x=[1,2,3,4,5]
+y=[2,4,6,8,10]
+print(x)
+print(y)
+import matplotlib.pyplot as plt
+plt.plot(x,y)
